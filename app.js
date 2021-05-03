d7ddd293380ff3916016b811b6980f9511bf4c42
unknown <puneethjnv@gmail.com>
2021-05-03 16:03:01 +0530

    Hashing included

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs=require('ejs');
 const mongoose=require('mongoose');
 const encrypt=require('mongoose-encryption');
 const app=express();
+const md5=require('md5');
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(express.static('public'));
@@ -14,8 +15,7 @@ const userSchema=new mongoose.Schema({
   email:String,
   password:String
 });
-const secret=process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secret,encryptedFields: ['password']});
+
 const User=new mongoose.model('User',userSchema);
 app.get('/',function(req,res){
   res.render('home');
@@ -28,7 +28,7 @@ app.get('/login',function(req,res){
 });
 app.post('/register',function(req,res){
   const userName=req.body.username;
-  const passWord=req.body.password;
+  const passWord=md5(req.body.password);
   const newUser=new User({
     email:userName,
     password:passWord
@@ -44,7 +44,7 @@ app.post('/register',function(req,res){
 });
 app.post('/login',function(req,res){
   const username=req.body.username;
-  const password=req.body.password;
+  const password=md5(req.body.password);
   User.findOne({email:username},function(err,foundUser){
     if (err) {
       console.log(err);
