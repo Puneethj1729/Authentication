c5ef36aef2c4c88b036c46e639009153fffa3b58
unknown <puneethjnv@gmail.com>
2021-05-04 23:00:00 +0530

    Submitting secrets using authentication

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ mongoose.connect('mongodb://localhost:27017/usersDB',{ useNewUrlParser: true, us
 const userSchema=new mongoose.Schema({
   email:String,
   password:String,
-  googleId:String
+  googleId:String,
+  secret:String
 });
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
@@ -73,14 +74,43 @@ app.get('/login',function(req,res){
   res.render('login');
 });
 app.get('/secrets',function(req,res){
+User.find({"secret":{$ne:null}},function(err,foundUsers){
+  if (err){console.log(err);}
+  else{
+    if(foundUsers){
+      res.render("secrets",{secretUsers:foundUsers});
+    }
+  }
+});
+
+});
+app.get('/submit',function(req,res){
   if (req.isAuthenticated())
   {
-    res.render('secrets');
+    res.render('submit');
   }
   else{
     res.redirect('/login');
   }
+});
+app.post('/submit',function(req,res){
+  const submittedSecret=req.body.secret;
+  console.log(req.user.id);
+  User.findById(req.user.id,function(err,foundUser){
 
+    if (err) {
+      console.log(err);
+    }
+    else{
+      if (foundUser){
+
+        foundUser.secret=submittedSecret;
+        foundUser.save(function(){
+          res.redirect('/secrets');
+        });
+      }
+    }
+  });
 });
 app.get('/logout',function(req,res){
   req.logout();
